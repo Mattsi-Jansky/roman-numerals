74f4c2f47fc7b5b17f8297561681b7a8c9951410
Mattsi <mattsi.jansky@codurance.com>
2020-02-22 17:27:05 +0000

    Naive implementation for parsing 1 and 2

--- roman-numerals.js
+++ roman-numerals.js
@@ -1,1 +1,9 @@
-module.exports = (i) => 'I'
+module.exports = (i) => {
+  let result = ''
+
+  for(let ii = 0; ii < i; ii++) {
+    result += 'I'
+  }
+
+  return result
+}
--- roman-numerals.spec.js
+++ roman-numerals.spec.js
@@ -1,7 +1,14 @@
 const parse = require('./roman-numerals')
 
-test('wat', () => {
+test('Parses 1 to I', () => {
   const result = parse(1)
 
   expect(result).toEqual('I')
 })
+
+test('parses 2 to II', () => {
+  const result = parse(2)
+
+  expect(result).toEqual('II')
+})
+
