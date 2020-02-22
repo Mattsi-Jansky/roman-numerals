72cf187b7ccd23f8656130fac5f546369d694140
Mattsi <mattsi.jansky@codurance.com>
2020-02-22 17:45:04 +0000

    Implement parsing 4 to IV

--- roman-numerals.js
+++ roman-numerals.js
@@ -1,9 +1,24 @@
 module.exports = (i) => {
   let result = ''
 
-  for(let ii = 0; ii < i; ii++) {
-    result += 'I'
+  const fives = i / 5
+
+  if(i > Math.ceil(5 / 2)) {
+    for(let ii = 0; ii < 5 - i; ii++) {
+      result += 'I'
+    }
+
+    for(let ii = 0; ii < fives; ii++) {
+      result += 'V'
+    }
   }
+  else {
+    for(let ii = 0; ii < i; ii++) {
+      result += 'I'
+    }
+  }
+
+  
 
   return result
 }
