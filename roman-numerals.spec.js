c114135b2c29a03f2ae0616431cd3a5192949eaa
Mattsi <mattsi.jansky@codurance.com>
2020-03-14 11:15:03 +0000

    Refactor and fix tests, 1 now failing

--- roman-numerals.spec.js
+++ roman-numerals.spec.js
@@ -1,73 +1,27 @@
 const { parse } = require('./roman-numerals')
 
-test('Parses 1 to I', () => {
-  const result = parse(1)
+const testCases = [
+  [1,'I'],
+  [2,'II'],
+  [3,'III'],
+  [4,'IV'],
+  [5,'V'],
+  [6,'VI'],
+  [7,'VII'],
+  [8,'VIII'],
+  [9,'IX'],
+  [10,'X'],
+  [11,'XI'],
+  [12,'XII'],
+]
 
-  expect(result).toEqual('I')
-})
+testCases.forEach(testCase => {
+  const from = testCase[0]
+  const to = testCase[1]
 
-test('parses 2 to II', () => {
-  const result = parse(2)
+  test(`Parses ${from} to ${to}`, () => {
+    const result = parse(from)
 
-  expect(result).toEqual('II')
-})
-
-test('parses 3 to III', () => {
-  const result = parse(3)
-
-  expect(result).toEqual('III')
-})
-
-test('parses 4 to IV', () => {
-  const result = parse(4)
-
-  expect(result).toEqual('IV')
-})
-
-test('parses 5 to V', () => {
-  const result = parse(5)
-
-  expect(result).toEqual('V')
-})
-
-test('parses 6 to VI', () => {
-  const result = parse(6)
-
-  expect(result).toEqual('VI')
-})
-
-test('parses 7 to VII', () => {
-  const result = parse(7)
-
-  expect(result).toEqual('VII')
-})
-
-test('parses 8 to VIII', () => {
-  const result = parse(7)
-
-  expect(result).toEqual('VII')
-})
-
-test('parses 9 to IX', () => {
-  const result = parse(7)
-
-  expect(result).toEqual('VII')
-})
-
-test('parses 10 to X', () => {
-  const result = parse(7)
-
-  expect(result).toEqual('VII')
-})
-
-test('parses 11 to XI', () => {
-  const result = parse(7)
-
-  expect(result).toEqual('VII')
-})
-
-test('parses 12 to XII', () => {
-  const result = parse(12)
-
-  expect(result).toEqual('XII')
+    expect(result).toEqual(to)
+  })
 })
